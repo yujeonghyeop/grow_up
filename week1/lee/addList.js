cc4654b7c8b53b93cafe1905f020c4ec38addcb6
dlwnstn0524 <ljs052450@gmail.com>
2021-10-12 00:48:32 +0900

    https://github.com/yujeonghyeop/grow_up.git

--- week1/lee/addList.js
+++ week1/lee/addList.js
@@ -1,3 +1,18 @@
-const inputBox = document.getElementById("inputBox")
-const addButton = document.getElementById("addButton")
-const list = document.getElementById("list")
+const inputBox = document.getElementById("inputBox");
+const addButton = document.getElementById("addButton");
+const toDoList = document.getElementById("list");
+
+addButton.addEventListener("click", function(){
+    const list = document.createElement("li");
+    toDolist.appendChild(list);
+
+    inputBox.value = " ";
+
+    list.addEventListener("click", function(){
+        list.style.textDecoration = "line-through";
+    })
+
+    list.addEventListener("dblclick", function(){
+        toDolist.removeChild(list);
+    })
+});
--- week1/lee/chrome/app.js
+++ week1/lee/chrome/app.js
@@ -1,1 +1,17 @@
-console.dir(document);
+const loginForm = document.getElementById("login-form"); 
+const loginInput = loginForm.querySelector("input");
+const loginButton  = loginForm.querySelector("button");
+
+function handleLogInBtnClicked(){
+     const username = loginInput.value;
+     if(username === ""){
+         alert("Please write your name!");
+     } else if (username.length > 15){
+        alert("Your name is too long!");
+     } else {
+         console.log(username);
+     }
+
+}
+
+loginButton.addEventListener("click", handleLogInBtnClicked);
