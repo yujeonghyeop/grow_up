e2ac8ac6162c01d1cd5b933db3a604327c56aba6
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2021-10-18 01:21:55 +0900

    week2.6

--- week2/you/app.js
+++ week2/you/app.js
@@ -1,1 +1,7 @@
-console.log("hello");
+const a = document.querySelector("h1"); 
+
+function clickhandle(){
+    const clickclass="clicked"
+    a.classList.toggle(clickclass)
+}
+a.addEventListener("click",clickhandle)
