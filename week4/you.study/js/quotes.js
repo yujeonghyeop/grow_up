66d4c5d65b6684565c55fdee2ef4989d7aff4b26
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2021-10-24 16:38:04 +0900

    week4.9.5

--- week4/you.study/js/quotes.js
+++ week4/you.study/js/quotes.js
@@ -11,6 +11,10 @@ const quotes =[ //적고싶은 글귀들을 배열로 저장한다.
     quote : "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life",
     author : "John 3:16",
 },
+{
+    quote : "For the lord your god is he that goeth with you, to fight for you against yout enemies, to save you",
+    author : "Deutoronomy 20:4",
+},
 ]
 const quote = document.querySelector("#quotes span:first-child")
 const author = document.querySelector("#quotes span:last-child") //html에 있는 곳에 적어두기 위해 queryselector로 가져온 다음 변수로 지정해준다. span last child
