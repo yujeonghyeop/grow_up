87ef1b77a4cecdbbe89848d01156a3ea353fecc3
dlwnstn0524 <ljs052450@gmail.com>
2021-10-27 19:50:11 +0900

    week5.3

--- week4/football league/js/junsu.js
+++ week4/football league/js/junsu.js
@@ -1,3 +1,11 @@
-const Team(){
+const team = {           /* Each team is meaning of each dormitory room.*/
+    name : '',           
+    w : 0,
+    d : 0,
+    l : 0,
+    value :0
+}
+
+const match = function(team1, team2){  /* Match means every game of each teams. */
     
 }
