29bda389c0ca63403f34166cdc50e1612fca0c5a
dlwnstn0524 <ljs052450@gmail.com>
2021-10-28 13:50:54 +0900

    week 5.4

--- week4/football league/js/calender.js
+++ week4/football league/js/calender.js
@@ -1,3 +1,6 @@
-import {team} from './team.js'
+import team from "team.js";
+
+
+
 console.log(1)
 console.log(team)
--- week4/football league/js/team.js
+++ week4/football league/js/team.js
@@ -1,4 +1,4 @@
-let team=[
+export let team=[
     {name:"C101",play:0,win:1,draw:1,lose:0,gf:0,ga:0,gd:0,point:0},
     {name:"C102",play:0,win:2,draw:2,lose:0,gf:0,ga:0,gd:0,point:0},
     {name:"C103",play:0,win:1,draw:3,lose:0,gf:0,ga:0,gd:0,point:0},
