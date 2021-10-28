96931a27b2e50492a0fe0806b644db8d1050400c
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2021-10-28 13:39:19 +0900

    week5.6.1

--- week4/football league/js/calender.js
+++ week4/football league/js/calender.js
@@ -1,14 +1,14 @@
 import team from './teaminf.js'
 const a =document.getElementById("match1")
-let inf = a.innerText.split('\n')
-let teamplay =inf[0].split('vs')
-for (let i=0;i<team.length;i++){
-    console.log(teamplay[0],team[i].name)
-    if (team[i].name===String(teamplay[0])){
-        console.log(1)
-        team[i].play+=1
-    }
-}
+// let inf = a.innerText.split('\n')
+// let teamplay =inf[0].split('vs')
+// for (let i=0;i<team.length;i++){
+//     console.log(teamplay[0],team[i].name)
+//     if (team[i].name===String(teamplay[0])){
+//         console.log(1)
+//         team[i].play+=1
+//     }
+// }
 console.log(team)
 
 export default team
