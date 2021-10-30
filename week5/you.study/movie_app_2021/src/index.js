b13495193017e629c31171ac356f510a69f2beca
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2021-10-30 19:53:03 +0900

    week5.11

--- week5/you.study/movie_app_2021/src/index.js
+++ week5/you.study/movie_app_2021/src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App />            
   </React.StrictMode>,
   document.getElementById('root')
 );
+//이 폴더에서 9번줄을 통해 html을 따오고, 7번을 통해 js를 html에 넣어준다. 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
