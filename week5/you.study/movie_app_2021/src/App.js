cd5dc30aef35ec6a8a2ea1b6886bec9a86767525
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2021-11-01 22:48:47 +0900

    week5.13

--- week5/you.study/movie_app_2021/src/App.js
+++ week5/you.study/movie_app_2021/src/App.js
@@ -1,16 +1,33 @@
 import react from "react";
 import axios from "axios"
+import Movie from "./Movies"
 class App extends react.Component{
   state={
-    isLoading : true
+    isLoading : true,
+    movies:[]
   }
-  componentDidMount(){
-    axios.get("https://yts.mx/api/v2/list_movies.json")
+  
+  getMovies = async () =>{
+    const {data: {data: {movies}}} = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating")
+    this.setState({movies:movies,isLoading:false})
   }
 
+  componentDidMount(){
+    this.getMovies()
+  }
   render(){
-    const {isLoading}=this.state
-    return <div>{isLoading ? "Loading..." : "We are Loading!"}</div>
+    const {isLoading, movies}=this.state
+    return <div>{isLoading ? "Loading..." : movies.map(movie=>{
+      console.log(movie)
+      return <Movie
+      key={movie.id}
+      id={movie.id}
+      year={movie.year}
+      title ={movie.title}
+      summary={movie.summary}
+      poster={movie.medium_cover_image}/>
+  })}
+    </div>
   }
 }
 
