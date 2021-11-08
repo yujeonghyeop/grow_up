7b775a3bb1880591505205629068c1233af3bc95
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2021-11-08 13:34:09 +0900

    week6.12

--- week6/you-react-project/src/App.js
+++ week6/you-react-project/src/App.js
@@ -1,10 +1,24 @@
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0)
+  const [keyword, setKeyword] = useState("")
+  const minus = () => {
+    setValue((prev)=>prev+1)
+  }
+  const onchange = (event) =>{
+    setKeyword(event.target.value)
+  }
+  useEffect(()=>{console.log("counter Change")},[counter])
+  useEffect(() => {console.log("input change!")},[keyword])
   return (
     <div>
-      <h1>hi kibum~</h1>
+      <input value ={keyword} onChange={onchange} type ="text" placeholder ="go input" />
+      <h1>{counter}</h1>
+      <button onClick={minus}>click</button>
     </div>
   );
 }
 
 export default App;
+ 
