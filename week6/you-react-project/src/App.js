416806e051e2316f6a8455fe996020ab51bbfcde
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2021-11-09 13:24:34 +0900

    week6.14

--- week6/you-react-project/src/App.js
+++ week6/you-react-project/src/App.js
@@ -1,20 +1,37 @@
 import { useEffect, useState } from "react";
-function Hello(){
-  useEffect(()=>{console.log("HI :)"); return ()=> console.log("Bye :(") },[])
-  return <h1>Hello</h1>
-}
+
 function App() {
-  const [show, showing] =useState(false)
-  const onclick = () => {
-    showing((prev)=>!prev)
-  }
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const onchange = (event) => setTodo(event.target.value);
+  const onsubmit = (event) => {
+    event.preventDefault();
+    console.log(todo);
+    if (todo === "") {
+      return;
+    }
+    setTodos((currentarray) => [todo, ...currentarray]); //...array 는 array안에 있는 요소들을 다 출력하게 해준다.
+    setTodo("");
+  };
   return (
     <div>
-      {show ? <Hello /> : null}
-      <button onClick = {onclick}>{show ? "Hide" : "Show"}</button>
+      <h1>My To Do ({todos.length})</h1>
+      <form onSubmit={onsubmit}>
+        <input
+          onChange={onchange}
+          value={todo}
+          type="text"
+          placeholder="write your todo"
+        ></input>
+        <button>add todo</button>
+      </form>
+      <hr />
+      <ul>
+        {todos.map((item, index) => (
+          <li key={index}> {item}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
-
 export default App;
- 
