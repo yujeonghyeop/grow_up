e9a813d0812d728d89e5a0b51f78974c72416e33
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2021-11-18 19:12:43 +0900

    week8.4

--- week7/you-react-project/src/component/Creatgame.js
+++ week7/you-react-project/src/component/Creatgame.js
@@ -1,24 +1,43 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useRef } from "react";
 
 function Creategame() {
-  const [lwin,setlwin]=useState(0)
+  const [lwin, setlwin] = useState(0);
+  const [llose,setllose] = useState(0);
+  const [ldraw,setldraw] = useState(0);
+  const [lwinscore,setlwinscore] = useState(0);
+  const [llosescore,setllosescore] = useState(0);
+  const [condition,setcondition] = useState(false)
+
   function onsubmit(event) {
     event.preventDefault();
-
-    async function geturl(){
-      const res = await fetch(`http://localhost:3002/Teams/${winteam.current.value}`)
-      const ress= await res.json()
-      setlwin(ress.win)
+    async function getwin() {   //win팀의 정보 설정
+      const res = await fetch(
+        `http://localhost:3002/Teams/${winteam.current.value}`
+      );
+      const ress = await res.json();
+      setlwin(ress.win);
+      
     }
-    geturl()
+    async function getlose() {    //lose팀의 정보 설정
+      const res = await fetch(
+        `http://localhost:3002/Teams/${loseteam.current.value}`
+      );
+      const ress = await res.json();
+      setllose(ress.lose);
+    }
+    getwin();
+    getlose();
+  }
+  function onclick(event){    //저장 할 시 win팀의 정보와 lose팀의 정보를 patch한다. 
+    event.preventDefault()
     fetch(`http://localhost:3002/Teams/${winteam.current.value}`, {
       method: "PATCH",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        win: lwin+1,
+        win: lwin + 1,
       }),
     }).then((res) => {
       if (res.ok) {
@@ -30,8 +49,8 @@ function Creategame() {
   const winscore = useRef(null);
   const loseteam = useRef(null);
   const losescore = useRef(null);
+  console.log(lwin);
   return (
-    
     <form onSubmit={onsubmit}>
       <div className="input_area">
         <label>win team</label>
@@ -50,6 +69,7 @@ function Creategame() {
         <input type="text" placeholder="0" ref={losescore}></input>
       </div>
       <button>저장</button>
+      <button onClick={onclick}>저장하시겠습니까?</button>
     </form>
   );
 }
