12870ffdc979328cfd3d3186b0252c314baeb9a2
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2021-11-18 22:38:14 +0900

    week8.5

--- week7/you-react-project/src/component/Creatgame.js
+++ week7/you-react-project/src/component/Creatgame.js
@@ -1,55 +1,90 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useRef } from "react";
 
 function Creategame() {
   const [lwin, setlwin] = useState(0);
-  const [llose,setllose] = useState(0);
-  const [ldraw,setldraw] = useState(0);
-  const [lwinscore,setlwinscore] = useState(0);
-  const [llosescore,setllosescore] = useState(0);
-  const [condition,setcondition] = useState(false)
+  const [llose, setllose] = useState(0);
+  const [drawa, setdrawa] = useState(0);
+  const [drawb, setdrawb] = useState(0);
+  const [wpoint, setwpoint] = useState(0);
+  const [lpoint, setlpoint] = useState(0);
+  const [condition, setcondition] = useState(false);
 
   function onsubmit(event) {
     event.preventDefault();
-    async function getwin() {   //win팀의 정보 설정
+    async function getwin() {
+      //win,lose 정보 설정
+      const win = await fetch(
+        `http://localhost:3002/Teams/${winteam.current.value}`
+      );
+      const winn = await win.json();
+      setlwin(winn.win + 1);
+      setdrawa(winn.draw);
+      setwpoint((winn.win + 1) * 3 + winn.draw);
+      const los = await fetch(
+        `http://localhost:3002/Teams/${loseteam.current.value}`
+      );
+      const lose = await los.json();
+      setllose(lose.lose + 1);
+      setdrawb(lose.draw);
+      setlpoint(lose.win * 3 + lose.draw);
+    }
+    async function getdraw() {
+      //draw 정보 설정
       const res = await fetch(
         `http://localhost:3002/Teams/${winteam.current.value}`
       );
       const ress = await res.json();
       setlwin(ress.win);
-      
-    }
-    async function getlose() {    //lose팀의 정보 설정
-      const res = await fetch(
+      setdrawa(ress.draw + 1);
+      setwpoint(ress.win * 3 + ress.draw + 1);
+
+      const ab = await fetch(
         `http://localhost:3002/Teams/${loseteam.current.value}`
       );
-      const ress = await res.json();
-      setllose(ress.lose);
+      const abc = await ab.json();
+      setllose(abc.lose);
+      setdrawb(abc.draw + 1);
+      setlpoint(ress.win * 3 + ress.draw + 1);
     }
-    getwin();
-    getlose();
+    winscore.current.value === losescore.current.value ? getdraw() : getwin();
+    setcondition(true);
   }
-  function onclick(event){    //저장 할 시 win팀의 정보와 lose팀의 정보를 patch한다. 
-    event.preventDefault()
+  function onclick(event) {
+    //저장 할 시 win팀의 정보와 lose팀의 정보를 patch한다.
+    event.preventDefault();
     fetch(`http://localhost:3002/Teams/${winteam.current.value}`, {
       method: "PATCH",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        win: lwin + 1,
+        win: lwin,
+        draw: drawa,
+        point: wpoint,
+      }),
+    });
+    fetch(`http://localhost:3002/Teams/${loseteam.current.value}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        lose: llose,
+        draw: drawb,
+        point: lpoint,
       }),
     }).then((res) => {
       if (res.ok) {
         alert("update 완료");
       }
     });
+    setcondition(false);
   }
   const winteam = useRef(null); //useRef는 돔에접근할수 있게 해준다.
   const winscore = useRef(null);
   const loseteam = useRef(null);
   const losescore = useRef(null);
-  console.log(lwin);
   return (
     <form onSubmit={onsubmit}>
       <div className="input_area">
@@ -69,7 +104,7 @@ function Creategame() {
         <input type="text" placeholder="0" ref={losescore}></input>
       </div>
       <button>저장</button>
-      <button onClick={onclick}>저장하시겠습니까?</button>
+      {condition ? <button onClick={onclick}>저장하시겠습니까?</button> : null}
     </form>
   );
 }
--- week7/you-react-project/src/component/Table.js
+++ week7/you-react-project/src/component/Table.js
@@ -3,20 +3,23 @@ import React, { useEffect, useState } from "react";
 function Table() {
   const [dorm, Setdorm] = useState([]);
 
-  useEffect(async () => {
-    const json = await (
-      await fetch("http://localhost:3002/Teams")
-    ).json();
-    json.sort(function (a, b) {
-      return a.point - b.point;
-    });
-    Setdorm(json.reverse());
+  useEffect(() => {
+    async function settable() {
+      const json = await fetch("http://localhost:3002/Teams");
+      const data = await json.json();
+      data.sort(function (a, b) {
+        return a.point - b.point;
+      });
+      Setdorm(data.reverse());
+    }
+    settable();
   }, []);
   return (
     <div>
       {dorm.map((dorm) => (
         <h1 key={dorm.id}>
-          {dorm.id} win : {dorm.win},point:{dorm.point}
+          {dorm.id} win : {dorm.win},lose : {dorm.lose},draw:{dorm.draw}, point:
+          {dorm.point}
         </h1>
       ))}
     </div>
